736640bb874a7a2e56afd7a0196e3a6602f5a8a0
Shubham SUnny <shubhamsahaninitkkr@gmail.com>
2020-08-14 08:08:29 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,76 +1,81 @@
 import React, { Component } from 'react';
 import './App.css';
-import TodoInput from "./components/TodoInput";
-import TodoList from "./components/TodoList";
+import TodoInput from './components/TodoInput';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import uuid from "uuid";
+import uuid from 'uuid';
 
 class App extends Component {
-  state={
-    items:[],
-    id:uuid(),
-    item:'',
-    editItem:false
-  }
+  state = {
+    items: [],
+    id: uuid(),
+    item: '',
+    editItem: false,
+  };
 
-  handleChange = (e) =>{
+  handleChange = (e) => {
     this.setState({
-      item : e.target.value
+      item: e.target.value,
     });
   };
 
-  handleSubmit = e =>{
+  handleSubmit = (e) => {
     e.preventDefault();
 
     const newItem = {
-      id:this.state.id,
-      title:this.state.item
-    }
+      id: this.state.id,
+      title: this.state.item,
+    };
 
-
-    const updatedItem =[...this.state.items,newItem];
+    const updatedItem = [...this.state.items, newItem];
 
     this.setState({
-      items:updatedItem,
-      item:"",
-      id:uuid(),
-      editItem:false
+      items: updatedItem,
+      item: '',
+      id: uuid(),
+      editItem: false,
     });
   };
 
-  clearList =()=>{
+  clearList = () => {
     this.setState({
-      items:[]
-    })
-  }
-
-  handleDelete = id => {
-    const filteredItems = this.state.items.filter(item =>item.id !==id);
-    this.setState({
-      items : filteredItems
+      items: [],
     });
-  }
+  };
 
-  handleEdit = id =>{
-    const filteredItems = this.state.items.filter(item =>item.id !==id);
+  handleDelete = (id) => {
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
+    this.setState({
+      items: filteredItems,
+    });
+  };
 
-    const selectedItems=this.state.items.find(item => item.id ===id);
+  handleEdit = (id) => {
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
+
+    const selectedItems = this.state.items.find((item) => item.id === id);
 
     this.setState({
-      items : filteredItems,
-      item:selectedItems.title,
-      editItem:true,
-      id:id
+      items: filteredItems,
+      item: selectedItems.title,
+      editItem: true,
+      id: id,
     });
-  }
-
+  };
 
   render() {
     return (
-  <div className="container py-5">
-    <TodoInput item={this.state.item} handleChange={this.handleChange} handleSubmit={this.handleSubmit} editItem={this.state.editItem}/>
-    <TodoList items={this.state.items} clearList={this.clearList} handleDelete={this.handleDelete} handleEdit={this.handleEdit}/>
-  </div>
+      <div className='container py-5'>
+        <TodoInput
+          item={this.state.item}
+          items={this.state.items}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          editItem={this.state.editItem}
+          clearList={this.clearList}
+          handleDelete={this.handleDelete}
+          handleEdit={this.handleEdit}
+        />
+      </div>
     );
   }
 }
